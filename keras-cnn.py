4f8673c91245ca6455164d62317acea841d1d58c
yvainePancmu <56243512+yvainePancmu@users.noreply.github.com>
2020-10-29 19:07:47 +0800

    Update keras-cnn.py

--- keras-cnn.py
+++ keras-cnn.py
@@ -1,9 +1,10 @@
-# coding=utf-8
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from keras import backend as K
+
+import mlflow
 import mlflow.keras
 
 mlflow.keras.autolog()
